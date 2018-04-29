47997415d40a4d8eaf6769331971660bc7a8bc61
stedyyulius <stedyyulius@gmail.com>
2018-04-29 11:10:11 +0700

    g

--- tes/App.js
+++ tes/App.js
@@ -1,55 +1,50 @@
 import React, { Component } from 'react';
-import axios from 'axios'
-import style from './Style'
+import TabNavigator from 'react-native-tab-navigator';
+import { StackNavigator } from 'react-navigation';
 import {
-  Platform,
   StyleSheet,
-  Text,
-  View,
   Image,
-  ScrollView
 } from 'react-native';
 
-const api = 'https://omgvamp-hearthstone-v1.p.mashape.com/cards'
+import HomeScreen from './components/Home'
+import ProfileScreen from './components/Profile'
+
+import Card from './components/Card'
+
+const Home = StackNavigator({
+  Home: { screen: HomeScreen },
+  Profile: { screen: ProfileScreen },
+});
 
 export default class App extends Component<Props> {
   constructor(props) {
     super(props)
     this.state={
-      cards: []
+      selectedTab: 'home',
     }
   }
-  componentDidMount() {
-    axios.get(api,{
-      headers: {
-        'X-Mashape-Key': 'H5m7TWh7somshXTc5fovt1JS3EFjp1xybJ9jsn5CIjEPnajN1a',
-      }
-    })
-    .then(res=> {
-      this.setState({
-        cards: res.data.Basic.slice(15,50)
-      })
-    })
-    .catch(err=>{
-      alert(err)
-    })
-  }
+
   render() {
     return (
-      <ScrollView style={styles.container}>
-        <View style={styles.cardRow}>
-        {(this.state.cards.length === 0)
-          ? <Text style={styles.textStyle}>Loading...</Text>
-          : (this.state.cards.map((c,i)=>
-                <View key={i}>
-                  <Image style={styles.image} source={{uri:c.img || 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/No_image_available_600_x_450.svg/600px-No_image_available_600_x_450.svg.png'}} />
-                </View>
-              ))
-        }
-        </View>
-      </ScrollView>
+      <TabNavigator>
+        <TabNavigator.Item
+          selected={this.state.selectedTab === 'home'}
+          title="Home"
+          renderIcon={() => <Image source={{uri:'http://www.stickpng.com/assets/images/588a6667d06f6719692a2d17.png'}} />}
+          renderSelectedIcon={() => <Image source={{uri:'http://www.stickpng.com/assets/images/588a6667d06f6719692a2d17.png'}} />}
+          // badgeText="1"
+          onPress={() => this.setState({ selectedTab: 'home' })}>
+          <Home />
+        </TabNavigator.Item>
+        <TabNavigator.Item
+          selected={this.state.selectedTab === 'profile'}
+          title="Cards"
+          renderIcon={() => <Image source={{uri:'https://d30y9cdsu7xlg0.cloudfront.net/png/20461-200.png'}} />}
+          renderSelectedIcon={() => <Image source={{uri:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmyPi-ebjlOYdiATAkxwHqr7v1KrdufeLFri4Wyzdb71tx6MUpbg'}} />}
+          onPress={() => this.setState({ selectedTab: 'profile' })}>
+          <Card />
+        </TabNavigator.Item>
+      </TabNavigator>
     );
   }
 }
-
-const styles = StyleSheet.create(style);
--- tes/Style.js
+++ tes/Style.js
@@ -1,6 +1,9 @@
 const style = {
   container: {
     padding: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
   },
   cardRow: {
     flexDirection: 'row',
@@ -13,6 +16,27 @@ const style = {
     margin: 15,
     width: 120,
     height: 120,
+  },
+  menuIcon: {
+    width: 100,
+    height: 100,
+    borderRadius: 4,
+    borderWidth: 5.5,
+    margin: 10,
+  },
+  menuIconRow: {
+    flexDirection: 'row',
+  },
+  menuIconCircle: {
+    width: 100,
+    height: 100,
+    borderRadius: 64,
+    borderWidth: 5.5,
+    margin: 10,
+  },
+  menuImage: {
+    width: 100,
+    height: 100,
   }
 }
 
